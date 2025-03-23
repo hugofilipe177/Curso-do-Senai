9f53298516eea4db4df64c6c0ebfa832266ad9dd
Hugo Filipe <199276967+hugofilipe177@users.noreply.github.com>
2025-03-23 11:07:28 -0300

    Update carousel.js
    melhora para criar o carrossel

--- js/carousel.js
+++ js/carousel.js
@@ -8,39 +8,50 @@ const carouselArr = [];
 
 //class Carousel
 class Carousel {
-    constructor(Imagem, Title, Url){
+    constructor(Imagem, Title, Url) {
         this.imagem = Imagem;
         this.title = Title;
         this.url = Url;
     }
-    
 
-    
-      
-    static Start(arr){
 
-        for(let i = 0; i < carouselArr.length; i++){
-        this.imagens = document.createElement('img');
-        this.imagens.setAttribute('src', carouselArr[i].imagem);
-        document.querySelector('#carousel').insertAdjacentElement('beforeend', this.imagens);
+
+
+    static Start(arr) {
+
+        for (let i = 0; i < carouselArr.length; i++) {
+            this.imagens = document.createElement('img');
+            this.imagens.setAttribute('src', carouselArr[i].imagem);
+            document.querySelector('#carousel').insertAdjacentElement('beforeend', this.imagens);
+            if(carouselArr[i].imagem)
         }
 
-        if(arr){
+const elemento = document.getElementById('carousel');
+elemento.style.display = 'none';
 
-            if(arr.length > 0){
+
+
+        this.titles = document.createElement('p');
+        this.titles.setAttribute('p', carouselArr[0].title);
+        document.querySelector('#carousel-title').insertAdjacentText('beforeend', this.titles);
+
+
+        if (arr) {
+
+            if (arr.length > 0) {
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel._interval = setInterval(function () { Carousel.Next(); }, 5000);
             }
-            
+
         } else {
             throw "Method Start need a Array Variable.";
         }
     }
-    
 
-    static Next(){
-        
+
+    static Next() {
+
     }
 };
