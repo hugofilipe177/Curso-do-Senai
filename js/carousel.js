7562a4d036eb19253f07a5f14d061d002c6bbedd
Hugo Filipe <kylon9867@gmail.com>
2025-03-24 13:49:33 -0300

    carrossel quase completo

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
-// Array para armazenar as instâncias do Carousel
+
 const carouselArr = [];
 
-// Classe Carousel
+
 class Carousel {
     constructor(Imagem, Title, Url) {
         this.imagem = Imagem;
@@ -10,16 +10,13 @@ class Carousel {
     }
 
     static Start(arr) {
-        // Verifica se o array foi passado e se não está vazio
         if ( arr.length > 0) {
-            Carousel._sequence = 0; // Inicia a sequência no primeiro item
-            Carousel._size = arr.length; // Define o tamanho do array
-            Carousel._arr = arr; // Armazena o array para uso posterior
+            Carousel._sequence = 0;
+            Carousel._size = arr.length; 
+            Carousel._arr = arr; 
 
-            // Exibe a primeira imagem e título
             Carousel.Next();
 
-            // Inicia o intervalo para alternar os itens a cada 5 segundos
             Carousel._interval = setInterval(function () {
                 Carousel.Next();
             }, 2000);
@@ -27,29 +24,26 @@ class Carousel {
             throw "Method Start needs an Array Variable.";
         }
     }
-
     static Next() {
-        // Obtém o elemento do carrossel
+
         const carouselElement = document.getElementById('carousel');
-        const titleElement = document.getElementById('carousel-title');
+        const UrlElement = document.getElementById ('carousel-title');
 
-        // Limpa o conteúdo atual do carrossel e do título
         carouselElement.innerHTML = '';
-        titleElement.innerHTML = '';
+        UrlElement.innerHTML ='';
+        
 
-        // Cria e exibe a próxima imagem
         const img = document.createElement('img');
         img.setAttribute('src', Carousel._arr[Carousel._sequence].imagem);
         carouselElement.appendChild(img);
 
-        // Exibe o próximo título
-        const title = document.createElement('p');
-        title.textContent = Carousel._arr[Carousel._sequence].title;
-        titleElement.appendChild(title);
+        const Url = document.createElement('a');
+        Url.href = Carousel._arr[Carousel._sequence].Url;
+        Url.textContent = Carousel._arr[Carousel._sequence].title;
+        UrlElement.appendChild(Url)
 
-        // Atualiza a sequência para o próximo item
+
         Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
     }
-}
-
-
+};
+  
