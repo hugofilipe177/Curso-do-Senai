444d596df06773ba40df97f517d01055d287cea1
Hugo Filipe <kylon9867@gmail.com>
2025-04-07 16:12:30 -0300

    desafio pronto

--- js/form.js
+++ js/form.js
@@ -1,10 +1,8 @@
-
-//class contato
-
 class contato {
-    constructor(nome,email,telefone,contato){
+    constructor(nome,email, cpf,telefone,contato){
     this.nome = nome;
     this.email = email;
+    this.cpf = cpf;
     this.telefone = telefone; 
     this.contato = contato; 
 }
@@ -14,19 +12,26 @@ function Post(form) {
 
   let data = new contato(form.elements.namedItem("nome").value,
             form.elements.namedItem("email").value, 
+            form.elements.namedItem("cpf").value,
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem('textarea').value,
             form.elements.namedItem("contato").value);
-  console.log(data);
+    console.table(data)
   form.reset();
 }
 
 function Enviar() {
-    
-    var nome = document.getElementById("nome");
+    const checkbox = document.getElementById('obrigatorio');
+    const nome = document.getElementById("nome");
+    const errocheck = document.getElementById("erro_checkbox");
+    const erroicon = '<i class="fa-light fa-circle-exclamation"></i>'
+
+    if(!checkbox.checked){
+        errocheck.innerHTML = `${erroicon} É Nescessário Confirmar Que Leu` ;
+    }
 
 
-    if (nome.value != "") {
+    if (nome.value != "" && checkbox.checked) {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
 
